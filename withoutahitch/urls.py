e075dfd0c74138e024820f91fc640c4defbd2ea6
Sagar B Hathwar <sagarbhathwar@gmail.com>
2017-11-14 14:31:09 +0530

    Removed Randomly uploaded files. Updated views and framework for html

--- withoutahitch/urls.py
+++ withoutahitch/urls.py
@@ -1,8 +1,26 @@
 from django.conf.urls import url
+from . import views
+
 
 # Useful when referencing views from urls.py
 app_name = "withoutahitch"
 
 urlpatterns = [
-    url('')
+    url(r"^$", views.IndexView.as_view(), name="index"),
+    url(r"^login/$", views.login_page, name="login_page"),
+    url(r"^register/$", views.register_page, name="register_page"),
+    url(r"^about/$", views.contact, name="contact"),
+
+    # A page dedicated to each vendor
+    url(r"^caterer/(?P<pk>[0-9]+)/$", views.CatererView.as_view(), name='caterer'),
+    url(r"^decorator/(?P<pk>[0-9]+)/$", views.DecoratorView.as_view(), name='decorator'),
+    url(r"^venue/(?P<pk>[0-9]+)/$", views.VenueView.as_view(), name='venue'),
+
+    # List all vendors
+    url(r"^caterers/$", views.Caterers.as_view(), name='caterers'),
+    url(r"^decorators/$", views.Decorators.as_view(), name='decorators'),
+    url(r"^venues/$", views.Venues.as_view(), name='venues'),
+
+    url(r'^plan/$', views.plan_event, name='plan_event')
+
 ]
--- withoutahitch/views.py
+++ withoutahitch/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import *
+
+
+def login_page(request):
+    return render(request, template_name="withoutahitch/login.html")
+
+
+def register_page(request):
+    return render(request, template_name="withoutahitch/register.html")
+
+
+def contact(request):
+    return render(request, template_name="withoutahitch/contact.html")
+
+
+class IndexView(generic.ListView):
+    template_name = "withoutahitch/index.html"
+    context_object_name = "latest_events"
+
+    def get_queryset(self):
+        return Event.objects.order_by("-date")[:5]
+
+
+class Venues(generic.ListView):
+    template_name = "withoutahitch/venues.html"
+    context_object_name = "venues"
+
+    def get_queryset(self):
+        return Venue.objects.all()
+
+
+class Decorators(generic.ListView):
+    template_name = "withoutahitch/decorators.html"
+    context_object_name = "decorators"
+
+    def get_queryset(self):
+        return Decorator.objects.all()
+
+
+class Caterers(generic.ListView):
+    template_name = "withoutahitch/caterers.html"
+    context_object_name = "caterers"
+
+    def get_queryset(self):
+        return Caterer.objects.all()
+
+
+class VenueView(generic.DetailView):
+    template_name = "withoutahitch/venue.html"
+    model = Venue
+
+
+class CatererView(generic.DetailView):
+    template_name = "withoutahitch/caterer.html"
+    model = Caterer
+
+
+class DecoratorView(generic.DetailView):
+    template_name = "withoutahitch/decorator.html"
+    model = Decorator
+
+
+def plan_event(request):
+    decorators = Decorator.objects.all()
+    venues = Decorator.objects.all()
+    caterers = Caterer.objects.all()
+
+    return render(request, "withoutahitch/plan.html",
+                  context={"decorators": decorators,
+                           "venues": venues,
+                           "caterers": caterers
+                           })
+
